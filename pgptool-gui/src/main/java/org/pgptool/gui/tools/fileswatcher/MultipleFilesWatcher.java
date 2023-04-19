fd8ba23ab46a5327deadbad43bd0acec7d2aa6a7
Sergey Karpushin <sergey.v.karpushin@gmail.com>
2023-04-19 16:15:32 +0300

    Fix #283: Build & migrate MultipleFilesWatcher to Apache commons I/O

--- pgptool-gui/src/main/java/org/pgptool/gui/tools/fileswatcher/MultipleFilesWatcher.java
+++ pgptool-gui/src/main/java/org/pgptool/gui/tools/fileswatcher/MultipleFilesWatcher.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * PGPTool is a desktop application for pgp encryption/decryption
- * Copyright (C) 2019 Sergey Karpushin
+ * Copyright (C) 2023 Sergey Karpushin
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,230 +36,87 @@ import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Logger;
-import org.summerb.utils.threads.RecurringBackgroundTask;
 
-// NOTE: Consider migrating to org.apache.commons.io.monitor.FileAlterationObserver, it appears we introduced bycle here
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
 
+/**
+ * @author Fredrick R. Brennan <copypaste@kittens.ph>
+ */
 public class MultipleFilesWatcher {
 	private static Logger log = Logger.getLogger(MultipleFilesWatcher.class);
 
 	private FilesWatcherHandler dirWatcherHandler;
 	private String watcherName;
-
-	private Map<WatchKey, BaseFolder> keys = new HashMap<>();
-	private Map<String, BaseFolder> baseFolders = new HashMap<>();
-
-	private Thread workerThread;
-	private WatchService watcher;
-
-	/**
-	 * since WatchService is not notifying us in case of parent folders removal AND
-	 * we don't want to overcomplicate code to watch ALL parent folders we're having
-	 * here another simple thread that will go and check folders existance
-	 */
-	private RecurringBackgroundTask dirsExistanceWatcherTask;
+	private FileAlterationMonitor monitor;
+	private Map<String, FileAlterationObserver> observers = new HashMap<>();
 
 	public MultipleFilesWatcher(FilesWatcherHandler dirWatcherHandler, String watcherName) {
 		this.dirWatcherHandler = dirWatcherHandler;
 		this.watcherName = watcherName;
+		this.monitor = new FileAlterationMonitor(1000);
 		startWatcher();
 	}
 
 	private void startWatcher() {
 		try {
-			watcher = FileSystems.getDefault().newWatchService();
-			workerThread = buildThreadAndStart();
-			dirsExistanceWatcherTask = new RecurringBackgroundTask(dirsExistanceWatcher, 2000);
-		} catch (Throwable t) {
-			throw new RuntimeException("failed to install watcher", t);
+			monitor.start();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to start the FileAlterationMonitor", e);
 		}
 	}
 
-	private Runnable dirsExistanceWatcher = new Runnable() {
-		@Override
-		public void run() {
-			synchronized (watcherName) {
-				for (BaseFolder bf : new ArrayList<>(baseFolders.values())) {
-					if (new File(bf.folder).exists()) {
-						continue;
-					}
-
-					for (String file : new ArrayList<>(bf.interestedFiles)) {
-						String filePathName = bf.folder + File.separator + file;
-						dirWatcherHandler.handleFileChanged(ENTRY_DELETE, filePathName);
-					}
-				}
-			}
-		}
-	};
-
 	public void watchForFileChanges(String filePathName) {
-		try {
-			String baseFolderStr = FilenameUtils.getFullPathNoEndSeparator(filePathName);
-			String relativeFilename = FilenameUtils.getName(filePathName);
+		String baseFolderStr = FilenameUtils.getFullPathNoEndSeparator(filePathName);
 
-			synchronized (watcherName) {
-				BaseFolder baseFolder = baseFolders.get(baseFolderStr);
-				if (baseFolder != null) {
-					log.debug("Parent directory is already being watched " + baseFolderStr
-							+ ", will just add file to watch " + relativeFilename);
-					baseFolder.interestedFiles.add(relativeFilename);
-					return;
+		synchronized (watcherName) {
+			if (observers.containsKey(baseFolderStr)) {
+				return;
+			}
+
+			FileAlterationObserver observer = new FileAlterationObserver(baseFolderStr);
+			observer.addListener(new FileAlterationListenerAdaptor() {
+				@Override
+				public void onFileChange(File file) {
+					dirWatcherHandler.handleFileChanged(ENTRY_MODIFY, file.getAbsolutePath());
 				}
 
-				Path path = Paths.get(baseFolderStr);
-				WatchKey key = path.register(watcher, ENTRY_DELETE, ENTRY_MODIFY, ENTRY_CREATE);
-				baseFolder = new BaseFolder(baseFolderStr, key, path, relativeFilename);
+				@Override
+				public void onFileCreate(File file) {
+					dirWatcherHandler.handleFileChanged(ENTRY_CREATE, file.getAbsolutePath());
+				}
 
-				keys.put(key, baseFolder);
-				baseFolders.put(baseFolderStr, baseFolder);
+				@Override
+				public void onFileDelete(File file) {
+					dirWatcherHandler.handleFileChanged(ENTRY_DELETE, file.getAbsolutePath());
+				}
+			});
 
-				log.debug("New watch key created for folder " + baseFolderStr + ", add first file " + relativeFilename);
-			}
-		} catch (Throwable t) {
-			log.error("Failed to watch file " + filePathName, t);
-			// not goinf to ruin app workflow
+			monitor.addObserver(observer);
+			observers.put(baseFolderStr, observer);
 		}
 	}
 
 	public void stopWatchingFile(String filePathName) {
-		try {
-			String baseFolderStr = FilenameUtils.getFullPathNoEndSeparator(filePathName);
-			String relativeFilename = FilenameUtils.getName(filePathName);
+		String baseFolderStr = FilenameUtils.getFullPathNoEndSeparator(filePathName);
 
-			synchronized (watcherName) {
-				BaseFolder baseFolder = baseFolders.get(baseFolderStr);
-				if (baseFolder == null) {
-					log.debug("No associated watchers found for " + baseFolderStr);
-					return;
-				}
-
-				baseFolder.interestedFiles.remove(relativeFilename);
-				log.debug("File is no longer watched in folder " + baseFolderStr + " file " + relativeFilename);
-				if (baseFolder.interestedFiles.size() > 0) {
-					return;
-				}
-
-				// NOTE: Decided to turn this off, because file might re-appear in case it's app
-				// re-created it. See #91, #75
-				// keys.remove(baseFolder.key);
-				// baseFolders.remove(baseFolder.folder);
-				// baseFolder.key.cancel();
-				// log.debug("Folder watch key is canceled " + baseFolderStr);
+		synchronized (watcherName) {
+			FileAlterationObserver observer = observers.get(baseFolderStr);
+			if (observer == null) {
+				return;
 			}
-		} catch (Throwable t) {
-			log.error("Failed to watch file " + filePathName, t);
-			// not goinf to ruin app workflow
+
+			monitor.removeObserver(observer);
+			observers.remove(baseFolderStr);
 		}
-	}
-
-	private Thread buildThreadAndStart() {
-		Thread ret = new Thread("FilesWatcher-" + watcherName) {
-			@Override
-			public void run() {
-				log.debug("FileWatcher thread started " + watcherName);
-				boolean continueWatching = true;
-				while (continueWatching) {
-					WatchKey key;
-					try {
-						idleIfNoKeysRegistered();
-						key = watcher.take();
-						// NOTE: Since we're watching only one folder we assume
-						// that there will be only one key for our folder
-					} catch (ClosedWatchServiceException cwe) {
-						log.info("ClosedWatchServiceException fired, stopping thread.");
-						return;
-					} catch (InterruptedException x) {
-						log.debug("FileWatcher thread stopped by InterruptedException");
-						return;
-					} catch (Throwable t) {
-						log.error("Unexpected exception while checking for updates on watched file", t);
-						return;
-					}
-
-					BaseFolder baseFolder = null;
-					synchronized (watcherName) {
-						baseFolder = keys.get(key);
-					}
-					if (baseFolder == null) {
-						key.cancel();
-						continue;
-					}
-
-					for (WatchEvent<?> event : key.pollEvents()) {
-						// Context for directory entry event is the file name of
-						// entry
-						WatchEvent<Path> ev = cast(event);
-						Path name = ev.context();
-						Path child = baseFolder.path.resolve(name);
-						String relativeFilename = FilenameUtils.getName(child.toString());
-						if (!baseFolder.interestedFiles.contains(relativeFilename)
-								&& !event.kind().equals(ENTRY_CREATE)) {
-							continue;
-						}
-
-						// print out event
-						log.debug("Watcher event: " + event.kind().name() + ", file " + child);
-						dirWatcherHandler.handleFileChanged(event.kind(), child.toString());
-					}
-
-					// reset key and remove from set if directory no longer
-					// accessible
-					boolean valid = key.reset();
-					if (!valid) {
-						synchronized (watcherName) {
-							keys.remove(key);
-							baseFolders.remove(baseFolder.folder);
-						}
-					}
-				}
-				log.debug("FileWatcher thread stopped " + watcherName);
-			}
-
-			private void idleIfNoKeysRegistered() throws InterruptedException {
-				while (true) {
-					synchronized (watcherName) {
-						if (!keys.isEmpty()) {
-							break;
-						}
-					}
-					Thread.sleep(500);
-				}
-			};
-		};
-		ret.start();
-		return ret;
 	}
 
 	public void stopWatcher() {
 		try {
-			workerThread.interrupt();
-			workerThread = null;
-			watcher.close();
-			watcher = null;
-			dirsExistanceWatcherTask.tearDown(1000);
-		} catch (Throwable t) {
-			log.error("Failed to gracefully close watcher service", t);
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-		return (WatchEvent<T>) event;
-	}
-
-	private static class BaseFolder {
-		String folder;
-		Path path;
-		List<String> interestedFiles = new ArrayList<>();
-		@SuppressWarnings("unused")
-		WatchKey key;
-
-		public BaseFolder(String folder, WatchKey key, Path path, String firstFile) {
-			this.folder = folder;
-			this.key = key;
-			this.path = path;
-			interestedFiles.add(firstFile);
+			monitor.stop();
+		} catch (Exception e) {
+			log.error("Failed to gracefully close FileAlterationMonitor", e);
 		}
 	}
 }
